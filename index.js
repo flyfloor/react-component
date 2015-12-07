b987c888ab3a7483817e64de294061fe40df5cbc
jerryshew <jerryshew@gmail.com>
2015-12-07 21:43:25 +0800

    fix export

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 'use strict';
 
-const React = require('react');
-const ReactDOM = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-const CheckBox = require("./lib/CheckBox.js");
-const Radio = require("./lib/Radio.js");
-const RadioGroup = require("./lib/RadioGroup.js");
-const CheckBoxGroup = require("./lib/CheckBoxGroup.js");
-const DropDown = require("./lib/DropDown.js");
-const Menu = require("./lib/Menu.js");
-const ConfirmBox = require("./lib/ConfirmBox.js");
-const Tooltip = require("./lib/Tooltip.js");
-const Modal = require("./lib/Modal.js");
-const Tab = require("./lib/Tab.js");
-const Pin = require("./lib/Pin.js");
-const Draggable = require("./lib/Draggable.js");
-const Carousel = require("./lib/Carousel.js");
-const Data = require("./lib/util/Data.js");
-const DocumentClickMixin = require("./lib/mixin/DocumentClickMixin.js");
-const IntervalMixin = require("./lib/mixin/IntervalMixin.js");
-const PopUpMixin = require("./lib/mixin/PopUpMixin.js");
-const ScrollMixin = require("./lib/mixin/ScrollMixin.js");
+var CheckBox = require("./lib/CheckBox.js");
+var Radio = require("./lib/Radio.js");
+var RadioGroup = require("./lib/RadioGroup.js");
+var CheckBoxGroup = require("./lib/CheckBoxGroup.js");
+var DropDown = require("./lib/DropDown.js");
+var Menu = require("./lib/Menu.js");
+var ConfirmBox = require("./lib/ConfirmBox.js");
+var Tooltip = require("./lib/Tooltip.js");
+var Modal = require("./lib/Modal.js");
+var Tab = require("./lib/Tab.js");
+var Pin = require("./lib/Pin.js");
+var Draggable = require("./lib/Draggable.js");
+var Carousel = require("./lib/Carousel.js");
+var Data = require("./lib/util/Data.js");
+var DocumentClickMixin = require("./lib/mixin/DocumentClickMixin.js");
+var IntervalMixin = require("./lib/mixin/IntervalMixin.js");
+var PopUpMixin = require("./lib/mixin/PopUpMixin.js");
+var ScrollMixin = require("./lib/mixin/ScrollMixin.js");
 
 module.exports = {
     React,

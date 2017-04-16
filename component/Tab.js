79be66639dee2ea7f708ec86aef781de3314eb35
jerryshew <jerryshew@gmail.com>
2017-04-16 20:17:34 +0800

    remove createClass: tab

--- component/Tab.js
+++ component/Tab.js
@@ -1,34 +1,18 @@
 const React = require('react')
+const Component = React.Component
+const PropTypes = require('prop-types')
 const klassName = require('./util/className')
-const PropTypes = React.PropTypes
 
-const Tab = React.createClass({
-    propTypes: {
-        onChange: PropTypes.func,
-        bottom: PropTypes.bool,
-        section: PropTypes.bool,
-        vertical: PropTypes.bool,
-    },
-    getDefaultProps() {
-        return {
-            className: '',
-            bottom: false,
-            vertical: false,
-            section: false,
-        };
-    },
-    getInitialState() {
-        const {current} = this.props;
-        return {current};
-    },
+class Tab extends Component {
+    constructor(props) {
+        super(props);
+        this.handleItemClick = this.handleItemClick.bind(this)
 
-    handleItemClick(index){
-        const {onChange} = this.props;
-        if (onChange) onChange(index);
-        this.setState({
-            current: index, 
-        });
-    },
+        const {current} = props
+        this.state = {
+            current
+        }
+    }
 
     componentWillReceiveProps(nextProps) {
         const {current} = this.props;
@@ -37,7 +21,15 @@ const Tab = React.createClass({
                 current: nextProps.current
             });
         }
-    },
+    }
+
+    handleItemClick(index){
+        const {onChange} = this.props;
+        if (onChange) onChange(index);
+        this.setState({
+            current: index, 
+        });
+    }
 
     makeTab(){
         let {children, style, className, vertical, section, bottom} = this.props;
@@ -83,13 +75,27 @@ const Tab = React.createClass({
         return (
             node
         )
-    },
+    }
 
     render() {
         return (
             this.makeTab()
         );
     }
-});
+}
+
+Tab.propTypes = {
+    onChange: PropTypes.func,
+    bottom: PropTypes.bool,
+    section: PropTypes.bool,
+    vertical: PropTypes.bool,
+}
+
+Tab.defaultProps = {
+    className: '',
+    bottom: false,
+    vertical: false,
+    section: false,
+}
 
 module.exports = Tab;

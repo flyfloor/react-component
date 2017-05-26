0eb9ed21e2516ad8c016b300282b44faab731316
jerryshew <jerryshew@gmail.com>
2017-05-26 17:06:33 +0800

    calendar onChange required

--- demo/example/CalendarDemo.jsx
+++ demo/example/CalendarDemo.jsx
@@ -7,7 +7,7 @@ export default class CalendarDemo extends Component {
     constructor(props){
         super(props);
         this.state = {
-            value: undefined
+            value: undefined,
         };
     }
 
@@ -24,19 +24,19 @@ export default class CalendarDemo extends Component {
                 <ul>
                     <li>
                         <h4>Default Calendar</h4>
-                        <Calendar/>
+                        <Calendar onChange={value => value}/>
                         <pre>
                             <code>
-                                {'<Calendar/>'}
+                                {'<Calendar onChange={this.handleChange.bind(this)}/>'}
                             </code>
                         </pre>
                     </li>
                     <li>
                         <h4>Calendar with given value</h4>
-                        <Calendar value={new Date("2016-04-20")}/>
+                        <Calendar value={new Date("2016-04-20")} onChange={value => value}/>
                         <pre>
                             <code>
-                                {'<Calendar value={new Date("2016-04-20")}/>'}
+                                {'<Calendar value={new Date("2016-04-20")} onChange={this.handleChange.bind(this)}/>'}
                             </code>
                         </pre>
                     </li>
@@ -46,43 +46,44 @@ export default class CalendarDemo extends Component {
                         <Calendar value={this.state.value} onChange={this.handleChange.bind(this)}/>
                         <pre>
                             <code>
-                                {'<Calendar value={this.state.value} onChange={handleChange}/>'}
+                                {'<Calendar value={this.state.value} onChange={this.handleChange}.bind(this)/>'}
                             </code>
                         </pre>
                     </li>
                     <li>
                         <h4>Calendar with begin and end</h4>
-                        <Calendar begin={new Date("2015-12-15")} end={new Date("2016-11-08")}/>
+                        <Calendar begin={new Date("2015-12-15")} 
+                            end={new Date("2016-11-08")} onChange={value => value} />
                         <pre>
                             <code>
-                                {'<Calendar begin={new Date("2015-12-15")} end={new Date("2016-11-08")}/>'}
+                                {'<Calendar begin={new Date("2015-12-15")} end={new Date("2016-11-08")} onChange={this.handleChange.bind(this)}/>'}
                             </code>
                         </pre>
                     </li>
                     <li>
                         <h4>Calendar with no preview</h4>
-                        <Calendar showPreview={false}/>
+                        <Calendar showPreview={false} onChange={value => value} />
                         <pre>
                             <code>
-                                {'<Calendar showPreview={false} />'}
+                                {'<Calendar showPreview={false}  onChange={this.handleChange.bind(this)}/>'}
                             </code>
                         </pre>
                     </li>
                     <li>
                         <h4>Calendar with type</h4>
                         <p>month:</p>
-                        <Calendar type='month' />
+                        <Calendar type='month' onChange={value => value} />
                         <pre>
                             <code>
-                                {'<Calendar type="month" />'}
+                                {'<Calendar type="month" onChange={this.handleChange.bind(this)} />'}
                             </code>
                         </pre>
                         <br/>
                         <p>year:</p>
-                        <Calendar type='year' />
+                        <Calendar type='year' onChange={value => value} />
                         <pre>
                             <code>
-                                {'<Calendar type="year" />'}
+                                {'<Calendar type="year" onChange={this.handleChange.bind(this)} />'}
                             </code>
                         </pre>
                         <br/>
